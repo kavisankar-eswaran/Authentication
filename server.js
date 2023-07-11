f410d4048cc902a2c8acbcc82cf6644666e9d9a6
Kavisankar Eswaran <84067576+kavisankar-eswaran@users.noreply.github.com>
2023-07-11 23:22:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 331;
 
 // Set up session middleware
 app.use(session({
